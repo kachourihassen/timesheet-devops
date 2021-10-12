c5fc49683e16d5ed0aaf931b655c7ae84b577da0
Hassen Kachouri <hassen.kachouri@esprit.tn>
2021-10-13 00:23:49 +0100

    Change user.java

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -45,7 +45,7 @@ public class User implements Serializable {
 		this.role = role;
 	}
 
-
+//hello
 	public User(Long id, String firstName, String lastName, Date dateNaissance, Role role) {
 		super();
 		this.id = id;
