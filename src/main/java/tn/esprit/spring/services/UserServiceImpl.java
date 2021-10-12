da4e99eb61ee148b8b35e1033ca3d421c0e400ff
Hassen Kachouri <hassen.kachouri@esprit.tn>
2021-10-13 00:16:56 +0100

    First Commit

---
+++ src/main/java/tn/esprit/spring/services/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entities.User;
+import tn.esprit.spring.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	private static final Logger l = LogManager.getLogger(UserServiceImpl.class);
+ 	 
+	 
+	
+	@Override
+	public List<User> retrieveAllUsers() { 
+		List<User> users = null; 
+		
+		//try {
+		l.info("In Method retrieverAllUsers :");
+		users=(List<User>) userRepository.findAll();
+		l.debug("connexion a la DB Ok :");
+		//int i = 1/0;
+		//l.debug("Je viens de lancer la divsion. " + i);
+		for(User user : users){
+			l.debug("user :" + user.getLastName());
+		}
+		l.info("Out of Methode retrieverAllUsers with Success");
+		//}
+		/*catch (Exception e) { l.error("Erreur dans getAllPrducts() : " + e); }*/
+		 
+		return users;
+	}
+
+
+	@Override
+	public User addUser(User u) {
+		l.info("In of Methode adduser:");
+		User u_saved = userRepository.save(u); 
+		l.info("Out of Methode adduser:");
+		return u_saved; 
+	}
+
+	@Override 
+	public User updateUser(User u) { 
+		l.info("In of Methode updateUser:"); 
+		User u_saved = userRepository.save(u); 
+		l.info("Out of Methode updateUser:");
+		return u_saved; 
+	}
+
+	@Override
+	public void deleteUser(String id) {
+		l.info("In of Methode deleteUser:");
+		userRepository.deleteById(Long.parseLong(id)); 
+		l.info("Out of Methode deleteUser:");
+	}
+
+	@Override
+	public User retrieveUser(String id) {
+		l.info("In of Methode retrieveUser:");
+		//User u =  userRepository.findById(Long.parseLong(id)).orElse(null);
+		User u =  userRepository.findById(Long.parseLong(id)).get(); 
+		l.info("Out of Methode retrieveUser:");
+		return u; 
+	}
+
+}
---
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTests.java
@@ -0,0 +1,72 @@
+package tn.esprit.spring.services;
+
+ 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.User;
+import tn.esprit.spring.services.IUserService;
+
+@SpringBootTest
+class UserServiceImplTest  {
+
+
+	@Autowired
+	IUserService us;
+	@Test
+	public void testRetrieveAllUsers() {
+	List<User> listUsers = us.retrieveAllUsers();
+	Assertions.assertEquals(2, listUsers.size());
+	}
+	public void testAddUser() throws ParseException{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = dateFormat.parse("2015-03-23");
+		User u = new User("hassen1","hassen1",d,Role.INGENIEUR);
+		User userAdded = us.addUser(u);
+		Assertions.assertEquals(u.getLastName(), userAdded.getLastName());
+		
+	}
+	public void testModifyUser() throws ParseException{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = dateFormat.parse("2015-03-23");
+		User u = new User(5L,"hassen1zzzz","hassen1",d,Role.INGENIEUR);
+		User userupdated = us.addUser(u);
+		Assertions.assertEquals(u.getLastName(), userupdated.getLastName());
+		
+	}
+	public void testDeleteUser(){
+		us.deleteUser("3");
+		Assertions.assertNull(us.retrieveUser("3"));
+	}
+	public void testRetrieveUser(){
+		User userRetrieved = us.retrieveUser("3");
+		Assertions.assertEquals(1l, userRetrieved.getId().longValue());
+	}
+	private static final Logger L = LogManager.getLogger(UserServiceImplTest.class);
+	
+	@Test
+	public void testAll(){
+		try{
+			L.info("In testAll()");
+			 testRetrieveAllUsers();
+			 testAddUser();
+			 testModifyUser();
+			 testDeleteUser();
+			 testRetrieveUser();
+			 L.info("Out of testAll()");
+		}catch(Exception e){
+		L.error("Out of testAll() wirh Errors :" + e);
+		}
+	}
+	
+}
